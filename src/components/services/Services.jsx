ee75d7e641083ffba0a071259fb9275a9f056899
Elana Strijbis <ehgestrijbis@gmail.com>
2022-08-18 23:03:10 +1000

    Add services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,110 @@
 import React from 'react'
 import './services.css'
-
+import { BiCheck } from 'react-icons/bi'
 function Services() {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>Placeholder heading 1</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of first card */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>Placeholder heading 2</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of second card */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>Placeholder heading 3</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem short lorem short lorem short lorem short</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of 3rd card */}
+      </div>
+    </section>
   )
 }
 
