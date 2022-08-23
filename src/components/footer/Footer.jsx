7bd015154de49ffba73f20d7f09c266f5e837eac
Elana Strijbis <ehgestrijbis@gmail.com>
2022-08-23 22:30:46 +1000

    Add footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+// import {FaFacebookF} from 'react-icons/fa'
+// import {FiInstagram} from 'react-icons/fi'
+// import {IoLogoTwitter} from 'react-icons/io'
 
-function Footer() {
+const Footer = () =>  {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>ELANA</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+{/* -----TO DO: ADD SOCIALS----- */}
+      {/* <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF /></a>
+        <a href="https://instagram.com"><FiInstagram /></a>
+        <a href="https://twitter.com"><IoLogoTwitter /></a>
+      </div> */}
+
+      <div className="footer__copyright">
+        <small>&copy; Elana Strijbis. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
