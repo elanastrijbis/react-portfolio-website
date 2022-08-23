017a85abe58bbf7130d2a56e7a1305fee4d62c61
Elana Strijbis <ehgestrijbis@gmail.com>
2022-08-23 22:29:05 +1000

    Add contact component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
@@ -11,22 +11,26 @@ const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Placeholder 1',
-    github: 'https://github.com/elanastrijbis',
-    demo: 'Demo placeholder 1'
+    title: 'Hello Dog',
+    subtitle: 'Ruby on Rails | Sass',
+    github: 'https://github.com/elanastrijbis/Hellodog',
+    demo: 'https://hellodog-app.herokuapp.com/'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Placeholder 2',
-    github: 'https://github.com/elanastrijbis',
-    demo: 'Demo placeholder 2'
+    title: 'Space Out',
+    subtitle: 'Ruby on Rails | Sass',
+    github: 'https://github.com/elanastrijbis/Space-0ut',
+    demo: 'https://young-garden-06123.herokuapp.com/'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Placeholder 3',
-    github: 'https://github.com/elanastrijbis',
+    title: 'Soundwave',
+    subtitle: 'HTML | CSS',
+
+    github: 'https://github.com/elanastrijbis/Soundwave',
     demo: 'Demo placeholder 3'
   },
   {
@@ -55,19 +59,20 @@ function Portfolio() {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+      <h2>Projects</h2>
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, subtitle, github, demo}) => {
             return (
               <article key={id} className='portfolio__item'>
                 <div className="portfolio__item-image">
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
+                <h4 className='portfolio__subtitle'>{subtitle}</h4>
                 <div className="portfolio__item-cta">
                   <a href={github} className='btn'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live</a>
                 </div>
               </article>
             )
